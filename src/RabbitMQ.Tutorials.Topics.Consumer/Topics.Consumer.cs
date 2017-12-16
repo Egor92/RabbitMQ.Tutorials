dbc892e4d261787bd78c56b4b370ef4ee136bf36
Novikov Egor <novikov.ea@mail.ru>
2017-12-16 13:33:41 +0300

    Tutorial 5. "Topics" passed

---
+++ src/RabbitMQ.Tutorials.Topics.Consumer/Topics.Consumer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Tutorials.Topics.Consumer
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Topics.Consumer";
+
+            Console.WriteLine("Input bindingKeys:");
+            var separator = new[]
+            {
+                ' '
+            };
+            string[] bindingKeys = Console.ReadLine()
+                                          .Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("topic_logs", "topic");
+                    var queueDeclareOk = channel.QueueDeclare();
+                    var queueName = queueDeclareOk.QueueName;
+
+                    foreach (var bindingKey in bindingKeys)
+                    {
+                        channel.QueueBind(queue: queueName, exchange: "topic_logs", routingKey: bindingKey);
+                    }
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+                        Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+                    };
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ src/RabbitMQ.Tutorials.Topics.Producer/Topics.Producer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Tutorials.Topics.Producer
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Topics.Producer";
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("topic_logs", "topic");
+
+                    while (true)
+                    {
+                        Console.Write("Input routingKey: ");
+                        var routingKey = Console.ReadLine();
+
+                        Console.Write("Input message: ");
+                        var message = Console.ReadLine() ?? string.Empty;
+
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "topic_logs", routingKey: routingKey, basicProperties: null, body: body);
+                        Console.WriteLine(" [x] Sent '{0}': '{1}'", routingKey, message);
+                    }
+                }
+            }
+        }
+    }
+}
